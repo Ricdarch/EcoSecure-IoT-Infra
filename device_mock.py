367755a5260522b5e0bfe4653708e1a7e134943a
Ricdarch <ricde.skorporation@gmail.com>
2026-04-30 19:28:49 +0200

    add device_mock.py file

---
+++ device_mock.py
@@ -0,0 +1,16 @@
+import random
+import time
+import json
+
+class SmartPlug:
+    def __init__(self, id, plug_time, connected, energy_consumption, is_used, idle_time):
+        self.id = id
+        self.plug_time = plug_time
+        self.connected = connected
+        self.energy_consumption = energy_consumption
+        self.is_used = is_used
+        self.idle_time = idle_time
+
+    def generate_data(self):
+        pass
+    
